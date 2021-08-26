6d177154611924121027be38190a3d04f5b49041
sergio <sergio3996@gmail.com>
2021-08-26 11:42:04 -0300

    Sendgrid

--- red_bicicletas/models/usuario.js
+++ red_bicicletas/models/usuario.js
@@ -67,7 +67,7 @@ usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
         if (err) { return console.log(err.message); }
 
         const mailOptions = {
-            from: 'no-reply@redbicicletas.com',
+            from: 'sergio3996@hotmail.com',
             to: email_destination,
             subject: 'Verificación de cuenta',
             text: 'Hola,\n\n' + 'Por favor, para verificar su cuenta haga click en este link: \n' + 'http://localhost:3000' + '\/token/confirmation\/' + token.token + '.\n'
